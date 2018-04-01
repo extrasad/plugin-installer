a432ee5077251e8b79d3b947d7172cca5f8463cb
Carlos Rivas <carlosr1765@gmail.com>
2018-04-01 00:10:12 -0400

    feat(plugin-installer): Added main feature for plugin installer

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 WP plugin to install plugins from a selected list.
 
 
-## TODO list
+## INSTRUCTIONS
+
+To use this plugin, you have to do the following steps: 
+
+1. Download or clone the repo, and install the WP plugin.
+2. Add the plugins to the array called 'plugins' in the plugin-installer.php. USE THE PLUGIN'S SLUG(if
+you don't use the plugin's slug, there will be a fatal error).
+3. Uncomment the line indicated in the plugin-installer.php file.
+4. After install all the plugins, proceed to comment the action line you uncommented before in the step 3, and delete
+all the plugins from the array 'plugins'. :!:DON'T LOAD THE PAGE AGAIN,
+OR NAVIGATE IN THE PAGE UNTIL COMMENT THE ACTION LINE:!:
+5. Refresh the page, and enjoy!!!
--- plugin-installer.php
+++ plugin-installer.php
@@ -19,3 +19,117 @@ if ( ! function_exists( 'add_action' ) ) {
     echo 'You don\'t have permission to access this file.';
     die;
   }
+
+class PluginInstaller{
+
+	private $plugins;
+	private $plugins_to_download;
+	private $install;
+	private $api;
+	private $plugin_folder;
+
+  public function __construct(){
+		include_once( ABSPATH . 'wp-admin/includes/plugin-install.php' ); //for plugins_api..
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		
+		/* Use this array to determinate the plugins that will be downloaded,
+		installed and activated. USE THE PLUGIN'S SLUG IN THE ARRAY. */
+		$this->plugins = array(
+			'jetpack', 
+			'uk-cookie-consent', 
+			'cookie-notice', 
+			'ultimate-modal'
+		);
+		
+		//Uncomment the line below if you want to test the plugin.
+		//add_action('init', $this->takePlugins($this->plugins));		
+	}
+
+  public function takePlugins($plugins)
+  {
+      $args = array(
+              'path' => ABSPATH.'wp-content/plugins/',
+              'preserve_zip' => false
+      );
+  
+			foreach($this->plugins as $plugin)
+      {
+				$this->api = plugins_api( 'plugin_information', array(
+					'slug' => $plugin,
+					'fields' => array(
+							'downloadlink' => true,
+							'slug' => true,
+					),
+				));
+        $this->PluginDownload($this->api->download_link, $args['path'].$this->api->slug.'.zip');
+				$this->PluginUnpack($args, $args['path'].$this->api->slug.'.zip');
+				$this->plugin_folder = ("/".$this->api->slug);
+				$var = get_plugins($this->plugin_folder);
+				foreach(array_keys($var) as $key)
+				{
+						$this->install = $this->plugin_folder."/".$key;
+				}
+				$this->PluginActivate($this->install);						
+			}			
+  }
+  public function PluginDownload($url, $path){
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      $data = curl_exec($ch);
+      curl_close($ch);
+      if(file_put_contents($path, $data)){
+        return true;
+			}else{
+			 return false;
+			}
+  }
+  public function PluginUnpack($args, $filename){
+  
+    if($zip = zip_open($filename)){
+      while($entry = zip_read($zip))
+      {
+        $file_check = substr(zip_entry_name($entry), -1) == '/' ? false : true;
+        $file_path = $args['path'].zip_entry_name($entry);
+        if($file_check){
+          if(zip_entry_open($zip,$entry,"r")){
+            $fstream = zip_entry_read($entry, zip_entry_filesize($entry));
+            file_put_contents($file_path, $fstream );
+            chmod($file_path, 0777);
+          }
+          zip_entry_close($entry);
+        }
+        else{
+          if(zip_entry_name($entry))
+          {
+            mkdir($file_path);
+            chmod($file_path, 0777);
+          }
+        }
+      }
+      zip_close($zip);
+      }
+    if($args['preserve_zip'] === false)
+    {
+      unlink($filename);
+    }
+  }
+  public function PluginActivate($install_path){
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    $get_current = get_option('active_plugins');
+    $plugin = plugin_basename(trim($install_path));
+
+    if(!in_array($plugin, $get_current)){
+      $get_current[] = $plugin;
+      sort($get_current);
+      do_action('activate_plugin', trim($plugin));
+      update_option('active_plugins', $get_current);
+      do_action('activate_'.trim($plugin));
+      do_action('activated_plugin', trim($plugin));
+      return true;
+    }
+    else
+    	return false;
+  }
+}
+
+new PluginInstaller();
